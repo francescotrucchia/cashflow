027f33ed58ed5bcb6d23aad26b9c104298825198
Francesco Trucchia <ft@ideato.it>
2013-01-03 01:36:15 +0100

    Added example

--- README.md
+++ README.md
@@ -1,3 +1,50 @@
 # Cashflow Library [![Build Status](https://secure.travis-ci.org/francescotrucchia/cashflow.png)](http://travis-ci.org/francescotrucchia/cashflow)
 
 Cashflow library is a set of php classes useful for manage cashflow operation as income, outcome and recurrent entries.
+
+<pre><code>
+<?php
+
+require_once __DIR__.'/../src/Cashflow/Autoload.php';
+
+$loader = new \Cashflow\ClassLoader('Cashflow', __DIR__ . '/../src');
+$loader->register();
+
+use Cashflow\Cashflow;
+use Cashflow\Outcome;
+use Cashflow\Income;
+
+$entries = array(
+    array(new \Cashflow\Income(),  new \DateTime(date('Y/06/10')), 'Balance', 1000),
+    array(new \Cashflow\Outcome(), new \DateTime(date('Y/06/11')), 'Credit card', 100),
+    array(new \Cashflow\Recurrent(new \Cashflow\Income()),  new \DateTime(date('Y/1/10')), 'Salary', 1500, new \DateInterval('P1M'), new \DateTime(date('Y/12/31'))),
+    array(new \Cashflow\Recurrent(new \Cashflow\Outcome()),  new \DateTime(date('Y/1/12')), 'Rent', 500, new \DateInterval('P1M'), new \DateTime(date('Y/12/31'))),
+);
+
+$cashflow = new Cashflow(new \DateTime(date('Y/1/1')), new \DateTime(date('Y/12/30')));
+$cashflow->import($entries);
+$cashflow->order();
+    
+$mask = "|%-10.10s |%-30.30s |%15s |%15s |\n";
+
+$output = sprintf($mask, 'Date', 'Name', 'Flow', 'Balance');
+
+foreach($cashflow->getRows() as $row){
+    $cashflow->updateAmount($row, $row->getSign());
+    $output .= sprintf(
+        $mask, 
+        $row->getDate()->format('Y-m-d'), 
+        $row->getName(), 
+        money_format('%.2n', $row->getSign()*$row->getAmount()), 
+        money_format('%.2n', $cashflow->getAmount())
+    );
+}
+
+$output .= PHP_EOL.
+  sprintf('Totale entrate: %s'.PHP_EOL, money_format('%.2n', $cashflow->getIncoming())).
+  sprintf('Totale uscite: %s'.PHP_EOL, money_format('%.2n', $cashflow->getOutcoming())).
+  sprintf('Guadagno: %s'.PHP_EOL, money_format('%.2n', $cashflow->getBalance()));
+
+echo $output;
+
+</code></pre>
--- bin/cashflow.php
+++ bin/cashflow.php
@@ -1,54 +1,43 @@
 <?php
 
-require_once __DIR__.'/../../../autoload.php';
+require_once __DIR__.'/../src/Cashflow/Autoload.php';
+
+$loader = new \Cashflow\ClassLoader('Cashflow', __DIR__ . '/../src');
+$loader->register();
 
 use Cashflow\Cashflow;
 use Cashflow\Outcome;
 use Cashflow\Income;
-use Cashflow\Output\Formatter;
 
-if(!file_exists($argv[1])){
-  die("File {$argv[1]} does not exists\n");
-  
-}
+$entries = array(
+    array(new \Cashflow\Income(),  new \DateTime(date('Y/06/10')), 'Balance', 1000),
+    array(new \Cashflow\Outcome(), new \DateTime(date('Y/06/11')), 'Credit card', 100),
+    array(new \Cashflow\Recurrent(new \Cashflow\Income()),  new \DateTime(date('Y/1/10')), 'Salary', 1500, new \DateInterval('P1M'), new \DateTime(date('Y/12/31'))),
+    array(new \Cashflow\Recurrent(new \Cashflow\Outcome()),  new \DateTime(date('Y/1/12')), 'Rent', 500, new \DateInterval('P1M'), new \DateTime(date('Y/12/31'))),
+);
 
-$entries = require_once($argv[1]);
-
-$cashflow = new Cashflow;
-$cashflow->setCredit(1000);
-
-foreach($entries as $entry){
-  $flow = $entry[0];
-  array_shift($entry);
-  $flow->fromArray($entry);
-  $cashflow->add($flow);
-  
-  
-}
-
+$cashflow = new Cashflow(new \DateTime(date('Y/1/1')), new \DateTime(date('Y/12/30')));
+$cashflow->import($entries);
 $cashflow->order();
     
-$mask = "|%-10.10s |%-30.30s |%10s |%10s |%10s |%10s |\n";
+$mask = "|%-10.10s |%-30.30s |%15s |%15s |\n";
 
-$output = sprintf($mask, 'Data', 'Nome', 'Entrate', 'Uscite', 'Stato', 'Con fido');
+$output = sprintf($mask, 'Date', 'Name', 'Flow', 'Balance');
 
 foreach($cashflow->getRows() as $row){
-  $cashflow->updateAmount($row, $row->getSign());
-
-  if($row instanceof Income){
-    $output .= sprintf($mask, $row->getDate()->format('d/m/y'), $row->getName(), $row->getAmount(), '', $cashflow->getAmount(), $cashflow->getCreditAmount());
-  }
-
-  if($row instanceof Outcome){
-    $output .= sprintf($mask, $row->getDate()->format('d/m/y'), $row->getName(), '', $row->getAmount(), $cashflow->getAmount(), $cashflow->getCreditAmount());
-  }
-
+    $cashflow->updateAmount($row, $row->getSign());
+    $output .= sprintf(
+        $mask, 
+        $row->getDate()->format('Y-m-d'), 
+        $row->getName(), 
+        money_format('%.2n', $row->getSign()*$row->getAmount()), 
+        money_format('%.2n', $cashflow->getAmount())
+    );
 }
 
-$output .= "\n";
-$output .= sprintf("Totale entrate: %s\n", $cashflow->getIncoming());
-$output .= sprintf("Totale uscite: %s\n", $cashflow->getOutcoming());
-$output .= sprintf("Guadagno: %s\n", $cashflow->getBalance());
+$output .= PHP_EOL.
+  sprintf('Totale entrate: %s'.PHP_EOL, money_format('%.2n', $cashflow->getIncoming())).
+  sprintf('Totale uscite: %s'.PHP_EOL, money_format('%.2n', $cashflow->getOutcoming())).
+  sprintf('Guadagno: %s'.PHP_EOL, money_format('%.2n', $cashflow->getBalance()));
 
 echo $output;
-
